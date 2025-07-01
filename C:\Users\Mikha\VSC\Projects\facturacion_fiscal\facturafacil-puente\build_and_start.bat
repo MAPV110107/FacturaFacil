48f8346daea0f4ee6eaaf4963b8088987c895378
Miguel Ángel Peña Vera <mapv110107@gmail.com>
2025-07-01 01:22:23 +0000

    el problema es que no existen en windows, la carpeta solo contiene otra

--- C:\Users\Mikha\VSC\Projects\facturacion_fiscal\facturafacil-puente\build_and_start.bat
+++ C:\Users\Mikha\VSC\Projects\facturacion_fiscal\facturafacil-puente\build_and_start.bat
@@ -1,1 +1,9 @@
-rem Este archivo debe ser eliminado. Usar los scripts de package.json en su lugar.
+
+@echo off
+rem Este archivo ha sido deprecado y no debe usarse.
+rem Utilice los comandos definidos en package.json en su lugar.
+rem Para instalar/construir: npm run setup && npm run build
+rem Para iniciar en produccion: npm start
+rem Este archivo puede ser eliminado de forma segura.
+echo "Este script esta obsoleto. Por favor, utilice los comandos de npm. Vea el contenido del script para mas detalles."
+pause
--- C:\Users\Mikha\VSC\Projects\facturacion_fiscal\facturafacil-puente\start_puente.bat
+++ C:\Users\Mikha\VSC\Projects\facturacion_fiscal\facturafacil-puente\start_puente.bat
@@ -1,1 +1,7 @@
-rem Este archivo debe ser eliminado. Usar 'npm run start:puente' en su lugar.
+
+@echo off
+rem Este archivo ha sido deprecado y no debe usarse.
+rem Utilice el comando "npm run start:puente" en su lugar.
+rem Este archivo puede ser eliminado de forma segura.
+echo "Este script esta obsoleto. Por favor, utilice 'npm run start:puente'. Vea el contenido del script para mas detalles."
+pause
