9f4608bdadb17a16cd180928f32f06f8875131fc
Miguel Ángel Peña Vera <mapv110107@gmail.com>
2025-07-01 01:29:00 +0000

    ya esta, he eliminado todos los archivos que comenzaban en "C:"

--- C:\Users\Mikha\VSC\Projects\facturacion_fiscal\facturafacil-puente\start_puente.bat
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem Este archivo ha sido deprecado y no debe usarse.
-rem Utilice el comando "npm run start:puente" en su lugar.
-rem Este archivo puede ser eliminado de forma segura.
-echo "Este script esta obsoleto. Por favor, utilice 'npm run start:puente'. Vea el contenido del script para mas detalles."
-pause
